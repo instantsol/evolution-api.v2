ee4d9168ee945ea48800bd5ed24edaf99eeef031
Pedro Howat <pedro@instant.com.br>
2026-02-04 09:05:53 -0300

    fix(contacts-history): fix display of scout conversations in contacts history
    ticket: Li0q8hFC

--- src/api/controllers/kwik.controller.ts
+++ src/api/controllers/kwik.controller.ts
@@ -449,9 +449,13 @@ export class KwikController {
 
   public async fetchContacts({ instanceName }: InstanceDto, body: any) {
     const owner = body?.where?.owner;
-    const instance = await this.prismaRepository.instance.findFirst({ where: { name: owner || instanceName } });
+    const instances = await this.prismaRepository.instance.findMany({ where: { name: owner || instanceName } });
 
-    const where: any = { instanceId: instance.id, lastmessagetimestamp: { not: null } };
+    //const where: any = { instanceId: instance.id, lastmessagetimestamp: { not: null } };
+    const where: any = {
+      instanceId: { in: instances.map((instance) => instance.id) },
+      lastmessagetimestamp: { not: null },
+    };
 
     if (body?.where?.id) where.remoteJid = body.where.id;
 
