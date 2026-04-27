ebe10fb32b486f407c115d8079fa65622eb6682f
lucas.moreira <lucas.moreira@instant.com.br>
2026-04-27 12:17:18 -0300

    fix: save sended message on S3 in merged branch

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -3929,7 +3929,8 @@ export class BaileysStartupService extends ChannelStartupService {
         }
       }
 
-      if ('messageContextInfo' in msg.message && Object.keys(msg.message).length === 1) {
+      const messageOwnKeys = Object.keys(msg.message);
+      if (messageOwnKeys.length === 1 && messageOwnKeys[0] === 'messageContextInfo') {
         this.logger.verbose('Message contains only messageContextInfo, skipping media processing');
         return null;
       }
